f41b43402b25bf82e9f750ca549314781bea329e
Anna <ann1989@list.ru>
2022-10-30 01:24:10 +0300

    Added delete_1

--- Psih_palata/delete.py
+++ Psih_palata/delete.py
@@ -1,37 +1,70 @@
 import csv
-from refresh import read_data
+from logg import log_error
+from global_v import file_personal, file_patients
+
+
+def read_worker():
+    with open('Psih_palata/Personal.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=';')
+        for rec in reader:
+            print(rec)
+
 
 def delete_worker():
-    
-    del_id = int(input())
-    result = []
-    data = read_data('Psih_palata/Personal.csv')
-    data.pop(del_id)
-    reader = csv.reader(data, delimiter=',')
-    for row in reader:
-        result.append(row)
-   
-    print('Данные о сотруднике удалены')
-    print(data)
-    print(result)
-#    writer = csv.writer(data, delimiter=',',quotechar=';', quoting=csv.QUOTE_MINIMAL)
-#    for row in writer:
-#            writer.writerow(result(zip(row)))
-    return result
+    with open('Psih_palata/Personal.csv', 'r', newline='\n', encoding='utf-8') as f:
+        try:
+            id_worker = input('Введите id сотрудника, информацию о котором хотите удалить: ')
+        except ValueError:
+            log_error()
+        found = 0
+        r = csv.reader(f, delimiter=";")
+        nrec = []
+        for rec in r:
+            if rec[0] == id_worker:
+                print('Запись удалена:', rec)
+                found = 1
+            else:
+                nrec.append(rec)
+
+        if found == 0:
+            print('Ошибка, пожалуйста, попробуйте снова')
+            f.close()
+        else:
+            with open('Psih_palata/Personal.csv', 'w', newline='', encoding='utf-8') as f:
+                w = csv.writer(f, delimiter=';')
+                w.writerows(nrec)
+# read()
+# delete_worker()
+
+def read_ward():
+    with open('Psih_palata/Patients.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=';')
+        for rec in reader:
+            print(rec)
 
 
 def delete_ward():
-    del2_id = int(input())
-    result2 = []
-    data2 = read_data('Psih_palata/Patients.csv')
-    data2.pop(del2_id)
-    reader2 = csv.reader(data2, delimiter=',')
-    for row in reader2:
-        result2.append(row)
-   
-    print('Данные о пациенте удалены')
-    print(data2)
-    print(result2)
+    with open('Psih_palata/Patients.csv', 'r', newline='\n', encoding='utf-8') as f:
+        try:
+            id_ward = input('Введите id пациента, информацию о котором хотите удалить: ')
+        except ValueError:
+            log_error()
+        found = 0
+        r = csv.reader(f, delimiter=";")
+        nrec = []
+        for rec in r:
+            if rec[0] == id_ward:
+                print('Запись удалена:', rec)
+                found = 1
+            else:
+                nrec.append(rec)
 
-    return result2
-
+        if found == 0:
+            print('Ошибка, пожалуйста, попробуйте снова')
+            f.close()
+        else:
+            with open('Psih_palata/Patients.csv', 'w', newline='', encoding='utf-8') as f:
+                w = csv.writer(f, delimiter=';')
+                w.writerows(nrec)
+# read()
+# delete_ward()
